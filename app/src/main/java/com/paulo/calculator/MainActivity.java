93814f424849b45d533c5bba1d9cc4320f28727b
Paulo Roberto <paulodiastst@hotmail.com>
2021-12-21 19:28:23 -0300

    Layout Calculator XML

---
+++ app/src/main/java/com/paulo/calculator/MainActivity.java
@@ -0,0 +1,25 @@
+package com.paulo.calculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button numeroZero,numeroUm,numeroDois,numeroTres,numeroQuatro,numeroCinco,numeroSeis,numeroSete,numeroOito,
+    numeroNove,ponto,soma,subtracao,multiplicacao,divisao,igual,botao_limpar;
+
+    private TextView txtExpressao,getTxtResultado;
+    private ImageView backspace;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        getSupportActionBar().hide();
+    }
+}
